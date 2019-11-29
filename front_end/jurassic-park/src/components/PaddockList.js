6f483344a6f62201fbc5a024b66f971485ecbbfb
Stan Tarnev <stan.tarnev@gmail.com>
2019-11-29 15:01:24 +0000

    added stuff

---
+++ front_end/jurassic-park/src/components/PaddockList.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import PaddockDetail from './PaddockDetail';
+
+const PaddockList =(props)=> {
+
+  const paddocks = props.data.map((paddock) => {
+    return (<PaddockDetail paddock={paddock}/>
+    )
+  })
+
+
+  return (
+    <div className="paddock-list">
+
+    {paddocks}
+    <hr/>
+    <a><button>Add a Paddock</button></a>
+    </div>
+  )
+  // <a onClick={this.addPaddock}><button>Add a Paddock</button></a>
+}
+
+export default PaddockList;
