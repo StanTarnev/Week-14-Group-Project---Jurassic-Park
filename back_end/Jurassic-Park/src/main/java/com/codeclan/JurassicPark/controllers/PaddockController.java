c9c0407d1d6aac4d05a9a44f764700ac40770cd9
sreenandini <mallelasreenandini2@gmail.com>
2019-11-29 21:17:18 +0000

    added projections

--- back_end/Jurassic-Park/src/main/java/com/codeclan/JurassicPark/controllers/PaddockController.java
+++ back_end/Jurassic-Park/src/main/java/com/codeclan/JurassicPark/controllers/PaddockController.java
@@ -1,27 +1,15 @@
 package com.codeclan.JurassicPark.controllers;
-
-import com.codeclan.JurassicPark.Repositories.DinosaurRepository;
 import com.codeclan.JurassicPark.Repositories.PaddockRepository;
-import com.codeclan.JurassicPark.models.Dinosaur;
-import com.codeclan.JurassicPark.models.Paddock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
-@RequestMapping(value="/paddocks")
+@RequestMapping(value = "/paddocks")
 public class PaddockController {
 
     @Autowired
     PaddockRepository paddockRepository;
 
-//    @GetMapping
-//    public List<Paddock> getAllPaddocks() {
-//        return paddockRepository.findAll();
-//
-//
-//    }
 }
