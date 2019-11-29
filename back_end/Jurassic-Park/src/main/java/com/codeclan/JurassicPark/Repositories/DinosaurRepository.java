2af5db5a18265ac09fbff9ff5ad7f8c6d9d40677
sreenandini <mallelasreenandini2@gmail.com>
2019-11-29 19:16:18 +0000

    fixed the create dinosaurs errors

--- back_end/Jurassic-Park/src/main/java/com/codeclan/JurassicPark/Repositories/DinosaurRepository.java
+++ back_end/Jurassic-Park/src/main/java/com/codeclan/JurassicPark/Repositories/DinosaurRepository.java
@@ -1,9 +1,6 @@
 package com.codeclan.JurassicPark.Repositories;
 import com.codeclan.JurassicPark.models.Dinosaur;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
 
 public interface DinosaurRepository extends JpaRepository<Dinosaur, Long> {
 }
