0957b15cd60c3f9cebd979c712b3414f51f61d67
sreenandini <mallelasreenandini2@gmail.com>
2019-12-04 08:12:53 +0000

    added dinosaurs images to dropdown and added required form

--- front_end/jurassic-park/src/components/dinosaurs/DinosaurForm.jsx
+++ front_end/jurassic-park/src/components/dinosaurs/DinosaurForm.jsx
@@ -3,38 +3,48 @@ import React from 'react';
 const DinosaurForm = (props) => {
 
   const options = props.paddocks.map((paddock, index) => {
-    return <option key={index} value={paddock._links.self.href}>{paddock.name} ({paddock.type})</option>
+    return <option key={index} value={paddock._links.self.href}>{paddock.name}</option>
   })
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const dinosaur={};
-    dinosaur.name = event.target.name.value;
-    dinosaur.species = event.target.species.value;
-    dinosaur.type = event.target.type.value;
-    dinosaur.gender = event.target.gender.value;
-    dinosaur.paddock = event.target.paddock.value;
-    props.handleDinosaurPost(dinosaur);
+    const dinosaur ={
+      "name": event.target.name.value,
+      "species": event.target.species.value,
+      "type": event.target.type.value,
+      "gender": event.target.gender.value,
+      "age": event.target.age.value,
+      "paddock": event.target.paddock.value
     }
+    props.handleDinosaurPost(dinosaur);
+
+  }
+
   return (
     <div>
       <h3>Add a new dinosaur</h3>
       <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Name" name="name"/>
-      <select name="species">Gender:
+      <input type="text" required="required" placeholder="Name" name="name"/>
+      <select required="required" name="species">Gender:
         <option value="Cerasinops">Cerasinops</option>
+        <option value="Leptoceratops">Leptoceratops</option>
+        <option value="Bagaceratops">Bagaceratops</option>
+        <option value="Protoceratops">Protoceratops</option>
+        <option value="Leaellynasaura">Leaellynasaura</option>
+        <option value="Dracorex">Dracorex</option>
         <option value="Archaeoceratops">Archaeoceratops</option>
         <option value="Microceratops">Microceratops</option>
       </select>
-      <select name="gender">Gender:
+      <input required="required" type="number" min="0" placeholder="Age" name="age"/>
+      <select required="required" name="gender">Gender:
         <option value="female">Female</option>
         <option value="male">Male</option>
       </select>
-      <select name="type">Type:
+      <select required="required" name="type">Type:
         <option value="herbivore">Herbivore</option>
         <option value="carnivore">Carnivore</option>
       </select>
-      <select name="paddock">
+      <select required="required" name="paddock">
         {options}
       </select>
       <button type="submit">Save</button>
